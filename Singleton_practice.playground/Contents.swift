41f65bb20980fe9814b64f83a05097eb88161fa7
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-09-15 16:38:22 +0900

    Player 인스턴스 생성

--- Singleton_practice.playground/Contents.swift
+++ Singleton_practice.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
+class Player {
+    static let player_stat = Player()
+    
+    var name: String?
+    var str: Int?
+    var dex: Int?
+    var vit: Int?
+    var hp: Int?
+    
+    private init() {}
+
+    private init(name: String?, str: Int?, dex: Int?, vit: Int?, hp: Int?) {
+        self.name = name
+        self.str = str
+        self.dex = dex
+        self.vit = vit
+        self.hp = hp
+    }
+}
+
+
 
